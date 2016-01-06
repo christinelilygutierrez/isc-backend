0b3f0072eaf184dda03abc0f506772107ce7b1cc
Jeff Tribble <jeffwtribble@gmail.com>
2016-01-05 19:23:14 -0700

    Add CORS middleware

--- app.js
+++ app.js
@@ -26,8 +26,20 @@ app.use(cookieParser());
 
 /************** Static files like js, CSS, images **************/
 // Reference all materials in the public directory
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
+// apply CORS middleware
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", (req.headers.origin) ? req.headers.origin : '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 /************** Routers for Web Pages **************/
 // Use the router for the webpages
